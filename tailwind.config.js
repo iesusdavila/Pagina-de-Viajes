2fc901a90d80ff2c2300ff8a50cdc4a6193f775f
iesusdavila <irdavila@espol.edu.ec>
2023-02-14 09:24:19 -0500

    Conceptos fundamentales de Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  corePlugins: {
+    focus: true,
+  },
+  darkMode: "class",
+};
