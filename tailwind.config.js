6b01fdfdb76ba6b8a9521144dc446c32263755ff
iesusdavila <irdavila@espol.edu.ec>
2023-02-16 00:37:27 -0500

    Header del proyecto acabado

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,19 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        gye1: 'url("./public/imgs/gye_1.jpeg")',
+        gye1: 'url("../public/imgs/fondos/fondo_ec_6.jpg")',
       },
-      backgroundColor: (theme) => ({
-        ...theme("colors"),
-        primario: "#cc2d4a",
-        secundario: "#8fa206",
-        terceario: "#61aec9",
-      }),
+      colors: {
+        primario: "#022534",
+        secundario: "#2b4534",
+        terceario: "#08546c",
+        cuaternario: "#1E0733",
+        oscuro: "#020203",
+        claro: "#a0bacc",
+      },
+      boxShadow: {
+        box: "-3px 3px 0px 0px #2c2cbf",
+      },
     },
   },
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
